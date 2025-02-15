2eb60a4ef858c82df42c992153eeb94431a1e26d
JoaoMadeiraxyz <jaodisc@gmail.com>
2025-02-15 14:30:11 -0300

    feat: add block drops and recipes

--- src/main/java/net/kilodev/firstmod/item/ModCreativeModeTabs.java
+++ src/main/java/net/kilodev/firstmod/item/ModCreativeModeTabs.java
@@ -21,6 +21,9 @@ public class ModCreativeModeTabs {
     public static final RegistryObject<CreativeModeTab> ALEXANDRITE_BLOCKS_TAB = CREATIVE_MODE_TABS.register("alexandrite_blocks_tab", () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.ALEXANDRITE_BLOCK.get())).withTabsBefore(ALEXANDRITE_ITEMS_TAB.getId()).title(Component.translatable("creativetab.firstmod.alexandrite_blocks")).displayItems((itemDisplayParameters, output) -> {
         output.accept(ModBlocks.ALEXANDRITE_BLOCK.get());
         output.accept(ModBlocks.RAW_ALEXANDRITE_BLOCK.get());
+
+        output.accept(ModBlocks.ALEXANDRITE_ORE.get());
+        output.accept(ModBlocks.ALEXANDRITE_DEEPSLATE_ORE.get());
     }).build());
 
     public static void register(IEventBus eventbus) {
