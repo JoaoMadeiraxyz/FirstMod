d903b0f8d14b174975b91dd74e953c4690619f18
JoaoMadeiraxyz <jaodisc@gmail.com>
2025-02-15 01:34:27 -0300

    feat: add alexandrite block and block of raw alexandrite

--- src/main/java/net/kilodev/firstmod/FirstMod.java
+++ src/main/java/net/kilodev/firstmod/FirstMod.java
@@ -1,6 +1,7 @@
 package net.kilodev.firstmod;
 
 import com.mojang.logging.LogUtils;
+import net.kilodev.firstmod.block.ModBlocks;
 import net.kilodev.firstmod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
@@ -35,6 +36,7 @@ public class FirstMod {
         MinecraftForge.EVENT_BUS.register(this);
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -52,6 +54,11 @@ public class FirstMod {
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
             event.accept(ModItems.ALEXANDRITE);
             event.accept(ModItems.RAW_ALEXANDRITE);
+        }
+
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(ModBlocks.ALEXANDRITE_BLOCK);
+            event.accept(ModBlocks.RAW_ALEXANDRITE_BLOCK);
         }
     }
 
